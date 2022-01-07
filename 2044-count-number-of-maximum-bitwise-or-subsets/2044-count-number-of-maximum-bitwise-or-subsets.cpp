d4f8c86874274f48ce1d92597693286b571cacfb
Amogh Nagar <amogh.1923cs1192@kiet.edu>
2022-01-07 13:20:57 +0530

    Time: 148 ms (23.74%), Space: 9.2 MB (32.68%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    int dp[17][1<<17];
-    // memset(dp,0,sizeof(dp));
-    int solve(int maxor,int cor,int i,vector<int> nums){
-        if(i==nums.size()){
-            return cor==maxor?1:0;
-        }
-        if(dp[i][cor]!=0){
-            return dp[i][cor];
-        }
-        return dp[i][cor]=solve(maxor,cor,i+1,nums)+solve(maxor,cor|nums[i],i+1,nums);
-    }
-    
     int countMaxOrSubsets(vector<int>& nums) {
+        map<int,int> m;
         int n=nums.size();
-        int maxor=0;
-        for(int i=0;i<nums.size();i++){
-            maxor|=nums[i];
+        for(int i=0;i<(1<<nums.size());i++){
+            int count=0;
+            for(int j=0;j<n;j++){
+                if(i&(1<<j)){
+                    count|=nums[j];
+                }
+            }
+            m[count]++;
         }
-        
-        return solve(maxor,0,0,nums);
+        return m.rbegin()->second;
     }
 };
