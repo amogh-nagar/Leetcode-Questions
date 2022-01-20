1c164b9adf1ef86f560c5ec86263b4519f933fe7
Amogh Nagar <amogh.1923cs1192@kiet.edu>
2022-01-20 19:28:19 +0530

    Time: 48 ms (63.04%), Space: 31.8 MB (41.39%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        int l=0,m=INT_MAX,pi=0,h=0;
         for(int i=0;i<arr.size();i++){
-            pq.push({abs(x-arr[i]),i});
+            if(m>(abs(arr[i]-x))){
+                m=abs(arr[i]-x);
+                pi=i;
+            }
         }
-        int c=0;
+        cout<<pi;
         vector<int> v;
-        while(!pq.empty() && c<k){
-            
-            pair<int,int> p=pq.top();
-            // cout<<p.second<<endl;
-            pq.pop();
-            v.push_back(arr[p.second]);
+        v.push_back(arr[pi]);
+        l=pi-1;
+        h=pi+1;
+        int c=1;
+        while((l>=0 || h<arr.size()) && c<k){
+            int y=INT_MAX,p=INT_MAX;
+            if(l>=0){
+                p=abs(arr[l]-x);
+               
+            }
+            if(h<arr.size()){
+                y=abs(arr[h]-x);
+                
+            }
+            if(p>y){
+                
+                v.push_back(arr[h]);
+                h++;
+            }else{
+                v.push_back(arr[l]);
+                l--;
+            }
             c++;
         }
         sort(v.begin(),v.end());
         return v;
+        
     }
 };
