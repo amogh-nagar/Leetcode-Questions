d1012e52022623975b97f4a009ed878a3db1b7b4
Amogh Nagar <amogh.1923cs1192@kiet.edu>
2022-01-27 13:42:07 +0530

    Time: 316 ms (63.91%), Space: 65.3 MB (83.21%) - LeetHub

--- 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,25 +1,59 @@
-class Solution {
-public:
-    int findMaximumXOR(vector<int>& nums) {
-        int mask=0,maxx=0;
-        for(int i=31;i>=0;i--){
-            mask|=(1<<i);
-            set<int> s;
-            for(int i=0;i<nums.size();i++){
-                s.insert(nums[i] & mask);
-            }
-            
-            
-            int newm=maxx | (1<<i);
-            
-            for(auto prefix:s){
-                if(s.find(prefix ^ newm)!=s.end()){
-                    maxx=newm;
-                    break;
-                }
-            }
-        }
-        return maxx;
+class Trie{
     
+   public: Trie* child[2];
+    
+    Trie(){
+        this->child[0]=NULL;
+        this->child[1]=NULL;
+    }
+};
+
+
+class Solution {
+
+    Trie *tnode;
+    
+    
+    public:
+    
+    void insert(int x){
+        Trie *t=tnode;
+        bitset<32> bt(x);
+        for(int i=31;i>=0;i--){
+            if(!t->child[bt[i]]){
+                t->child[bt[i]]=new Trie();
+                
+            }
+            t=t->child[bt[i]];
+        }
+    }
+    
+    int maxxor(int x){
+       Trie *t=tnode;
+       bitset<32> bt(x);
+    int ans=0;
+        for(int j=31;j>=0;j--){
+        if(t->child[!bt[j]]){
+            ans|=(1<<j);
+            t=t->child[!bt[j]];
+        }else{
+            t=t->child[bt[j]];
+        }
+    }
+        return ans;
+    }
+    
+    int findMaximumXOR(vector<int>& nums) {
+        tnode=new Trie();  
+        
+        for(auto x:nums){
+              insert(x);
+          }
+        
+        int ans=INT_MIN;
+        for(auto x:nums){
+            ans=max(ans,maxxor(x));
+        }
+        return ans;
     }
 };
