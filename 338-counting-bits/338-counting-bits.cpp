207b81a3cbda7e244d959f078152a2f9283c418b
Amogh Nagar <amogh.1923cs1192@kiet.edu>
2022-03-01 11:45:44 +0530

    Time: 7 ms (62.32%), Space: 8.5 MB (21.58%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        if(n==0){
-            return {0};
-        }
-        if(n==1){
-            return {0,1};
-        }
         vector<int> res;
         res.push_back(0);
-        res.push_back(1);
-        for(int i=2;i<=n;i++){
-            int c2=log2(i);
-            c2=(1<<c2);
-            res.push_back(res[i-c2]+1);
-            
+        for(int i=1;i<=n;i++){
+            res.push_back(res[i/2] + (i&1));
         }
         return res;
     }
