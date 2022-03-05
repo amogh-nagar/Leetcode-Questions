9cf8cc773817d5e1309a9ffeee05fe585d8a6eb4
Amogh Nagar <amogh.1923cs1192@kiet.edu>
2022-03-05 11:20:11 +0530

    Time: 4 ms (94.57%), Space: 9.4 MB (74.40%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
+        int n=nums.size();
         int mx=*max_element(nums.begin(),nums.end());
         vector<int> freq(mx+1,0),dp(mx+1,0);
-        for(int i=0;i<nums.size();i++){
+        for(int i=0;i<n;i++){
             freq[nums[i]]+=nums[i];
         }
-        dp[0]=freq[0];
         dp[1]=freq[1];
-        for(int i=2;i<mx+1;i++){
-            dp[i]=max(dp[i-2]+freq[i],dp[i-1]);
+        for(int i=2;i<=mx;i++){
+            dp[i]=max(freq[i]+dp[i-2],dp[i-1]);
         }
-        
-        return dp[mx];
-        
+        return dp[mx];    
     }
 };
