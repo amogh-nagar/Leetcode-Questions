6559200413530b6541d88de24e04cb29563a6fc4
Amogh Nagar <amogh.1923cs1192@kiet.edu>
2022-03-13 11:57:40 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (51.50%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+                st.push(s[i]);
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                switch(s[i]){
+                    case ']': if(st.top()!='['){
+                                return false;
+                               }
+                              st.pop();
+                              break;
+                    case ')': if(st.top()!='('){
+                                return false;
+                               }
+                              st.pop();
+                              break;
+                    case '}': if(st.top()!='{'){
+                                return false;
+                               }
+                              st.pop();
+                              break;    
+                }
+            }
+        }
+        return st.empty();
+    }
+};
