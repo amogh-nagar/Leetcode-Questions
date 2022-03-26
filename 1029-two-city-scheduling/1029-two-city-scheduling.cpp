365bc1974892fab4f195a4d1210302dc86c32cba
Amogh Nagar <amogh.1923cs1192@kiet.edu>
2022-03-26 10:27:51 +0530

    Time: 18 ms (10.60%), Space: 12.3 MB (7.97%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-    // vector<vector<vector<int>>> dp;
-    int dp[101][51][51];
-    int solve(int a,int b,int i,vector<vector<int>>& c){
-        if(i>=c.size()){
-            return 0;
+    int dp[101][101][101];
+    int solve(vector<vector<int>>& c){
+        int n=c.size();
+        dp[0][0][1]=c[0][1];
+        dp[0][1][0]=c[0][0];
+        for(int i=1;i<n;i++){
+            dp[i][0][i+1]=dp[i-1][0][i]+c[i][1];
+            dp[i][i+1][0]=dp[i-1][i][0]+c[i][0];
+            for(int j=1,k=i;j<=i&&k>0;j++,k--){
+                dp[i][j][k]=min(dp[i-1][j][k-1]+c[i][1],dp[i-1][j-1][k]+c[i][0]);   
+                
+            }
         }
-        if(dp[i][a][b]!=-1){
-            return dp[i][a][b];
-        }
-        int x=INT_MAX,y=INT_MAX;
-        if(a>0){
-            x=solve(a-1,b,i+1,c)+c[i][0];
-        }
-        if(b>0){
-            y=solve(a,b-1,i+1,c)+c[i][1];
-        }
-        return dp[i][a][b]=min(x,y);
+        return dp[n-1][n/2][n/2];
     }
     
     int twoCitySchedCost(vector<vector<int>>& costs) {
         int n=costs.size();
-        // dp=vector<vector<vector<int>>> (n,vector<vector<int>>(n/2,vector<int>(n/2,-1)));
-        memset(dp,-1,sizeof(dp));
-        solve(n/2,n/2,0,costs);
-        return dp[0][n/2][n/2];
+        
+        memset(dp,0,sizeof(dp));
+        
+        return solve(costs);
     }
 };
