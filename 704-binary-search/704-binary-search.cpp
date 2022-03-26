cfe27e7da5b6ba0468c06570f681225047c4feae
Amogh Nagar <amogh.1923cs1192@kiet.edu>
2022-03-26 09:16:24 +0530

    Time: 58 ms (36.58%), Space: 27.4 MB (92.40%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l=0,h=nums.size()-1;
+        while(l<=h){
+            int m=(l+h)/2;
+            if(nums[m]==target){
+                return m;
+            }
+            else if(nums[m]>target){
+                h=m-1;
+            }else{
+                l=m+1;
+            }
+        }
+        return -1;
+    }
+};
