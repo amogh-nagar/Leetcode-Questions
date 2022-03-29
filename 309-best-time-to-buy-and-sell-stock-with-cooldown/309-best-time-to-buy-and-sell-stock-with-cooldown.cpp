c8ca3202c232356d31ae600af28cd8ad51bac99e
Amogh Nagar <amogh.1923cs1192@kiet.edu>
2022-03-29 23:40:21 +0530

    Time: 9 ms (36.43%), Space: 11.2 MB (58.93%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -2,16 +2,13 @@ class Solution {
     int dp[5001];
 public:
     int solve(vector<int> &prices, int n, int ind){
-        if (ind>=n) return 0;
-        if (dp[ind]!=-1) return dp[ind];
-        int max_val=0;
-        for (int i=ind+1;i<n;i++){
-            if (prices[ind]<prices[i]){
-                max_val=max(max_val, prices[i]-prices[ind]+solve(prices,n,i+2));
-            }
+        int maxi=prices[n-1];
+        dp[n-1]=0;
+        for (int i=n-2;i>=0;i--){
+            maxi=max(maxi,prices[i]+(i+2<n ? dp[i+2] : 0));
+            dp[i]=max(dp[i+1],maxi-prices[i]);
         }
-        max_val=max(max_val,solve(prices,n,ind+1));
-        return dp[ind]=max_val;
+        return dp[0];
     }
     
     int maxProfit(vector<int>& prices) {
