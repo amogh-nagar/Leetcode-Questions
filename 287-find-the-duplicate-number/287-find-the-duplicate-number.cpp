ce2b86f2e095387910f9f8551f4738ac4cd6d9ad
Amogh Nagar <amogh.1923cs1192@kiet.edu>
2022-03-29 10:30:50 +0530

    Time: 256 ms (17.24%), Space: 61.2 MB (61.36%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int l=1,ans=0,h=nums.size()-1;
-        while(l<=h){
-            int m=(l+h)/2;
-            int c=0;
-            for(int i=0;i<nums.size();i++){
-                if(nums[i]<=m){
-                    c++;
-                }
-            }
-            if(c<=m){
-                l=m+1;
-            }else{
-                ans=m;
-                h=m-1;
-            }
-        }
-        return ans;
+        int mask=0;
+        for(int i=0;i<32;i++){
+           int orgc=0,ac=0;
+           for(int j=0;j<nums.size();j++){
+               if((1<<i) & j){
+                   orgc++;
+               }
+               if((1<<i) & nums[j]){
+                   ac++;
+               }
+           }
+           if(ac>orgc){
+               mask=mask | (1<<i);
+           }
+       } 
+       return mask; 
     }
 };
