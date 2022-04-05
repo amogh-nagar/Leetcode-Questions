5cce9ed46278f5e2543c8a6fe289be4b70772e1e
Amogh Nagar <amogh.1923cs1192@kiet.edu>
2022-04-05 18:10:44 +0530

    Time: 3 ms (56.07%), Space: 6.8 MB (14.40%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        int n=nums.size(); 
+        vector<unsigned long long> dp(target+1,0);
+        dp[0]=1;
+        for(int i=1;i<=target;i++){
+            for(auto x:nums){
+                if(i>=x){
+                    dp[i]+=dp[i-x];
+                }
+                
+            }
+        }
+        return dp[target];
+    }
+};
