dcd5d3607eec1fcabca694cdfcbf5f57770381c6
Amogh Nagar <amogh.1923cs1192@kiet.edu>
2022-05-03 12:34:28 +0530

    Time: 46 ms (24.34%), Space: 11.1 MB (57.52%) - LeetHub

--- 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.cpp
+++ 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.cpp
@@ -2,17 +2,11 @@ class Solution {
 public:
     typedef long long int ll;
     long long countVowels(string word) {
-        vector<ll> dp(word.length()+1,0);
-        for(int i=1;i<=word.length();i++){
-            if(word[i-1]=='a'||word[i-1]=='e'||word[i-1]=='i'||word[i-1]=='o'||word[i-1]=='u'){
-                dp[i]=dp[i-1]+i;
-            }else{
-                dp[i]=dp[i-1];
-            }
-        }
         ll s=0;
-        for(auto x:dp){
-            s+=x;
+        for(int i=0;i<word.length();i++){
+            if(word[i]=='a'||word[i]=='e'||word[i]=='i'||word[i]=='o'||word[i]=='u'){
+                s+=((ll)(word.length()-i)*i+(word.length()-i));
+            }
         }
         return s;
     }
