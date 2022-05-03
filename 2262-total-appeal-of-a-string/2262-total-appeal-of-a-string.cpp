9899255d21fd18a1ec3aafd28ca08a5278c9c81c
Amogh Nagar <amogh.1923cs1192@kiet.edu>
2022-05-03 14:18:42 +0530

    Time: 30 ms (88.88%), Space: 10.6 MB (60.17%) - LeetHub

--- 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.cpp
+++ 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     long long appealSum(string s) {
-        long long ans=0,c=0;
-        vector<int>prev(26,-1);
-        map<char,int> m;
+        long long ans=0;
+        vector<int> prev(26,-1);
         for(int i=0;i<s.length();i++){
-            c+=(i-prev[s[i]-'a']);
+            ans+=((i-prev[s[i]-'a'])*(s.length()-i));
             prev[s[i]-'a']=i;
-            ans+=c;
         }
         return ans;
     }
