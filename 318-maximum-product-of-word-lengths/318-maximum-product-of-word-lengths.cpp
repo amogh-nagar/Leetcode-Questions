853ae2e3e0df82a5745319d940a031a2da14c1e6
Amogh Nagar <amogh.1923cs1192@kiet.edu>
2022-05-29 17:58:37 +0530

    Time: 105 ms (41.00%), Space: 17.2 MB (36.12%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
+    typedef long long int ll;
+    typedef long double ld;
+    ll max(ll a , ll b){if(a > b) return a ; return b ;}
+    ll min(ll a , ll b){if(a < b) return a ; return b ;}
+
     int maxProduct(vector<string>& words) {
-        vector<unordered_set<char>> v(words.size()); 
-        for(int i=0;i<words.size();i++){
-             for(auto x:words[i]){
-                 v[i].insert(x);
-             }
-         }
         int maxi=0;
+        vector<ll> v;
+        for(auto x:words){
+            ll c=0;
+            for(int i=0;i<x.size();i++){
+                c=c|(1<<(x[i]-'a'));
+            }
+            v.push_back(c);
+        }
         for(int i=0;i<words.size();i++){
-             for(int j=i+1;j<words.size();j++){
-                int f=0;
-                 for(char x='a';x<='z';x++){
-                   if(v[i].find(x)!=v[i].end() && v[j].find(x)!=v[j].end()){
-                       f=1;
-                       break;
-                   } 
+            for(int j=i+1;j<words.size();j++){
+                if(!(v[i] & v[j])){
+                    maxi=max(maxi,words[i].size()*words[j].size());
                 }
-                 if(!f){
-                maxi=max(maxi,(int)words[i].length()*(int)words[j].length());
-                 }
-             }
-         }
+            }
+        }
         return maxi;
     }
 };
