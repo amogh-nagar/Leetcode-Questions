3d91ad2410906700fe70dd9aaa28b1375f6eb54d
Amogh Nagar <amogh.1923cs1192@kiet.edu>
2022-06-03 09:09:28 +0530

    Time: 656 ms (38.98%), Space: 148 MB (79.01%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -1,39 +1,35 @@
 class NumMatrix {
 public:
-    vector<vector<int>> v;
-    vector<vector<int>> dp;
+    vector<vector<int>> matrix;
     NumMatrix(vector<vector<int>>& matrix) {
-        v=matrix;
-        int n=matrix.size(),m=matrix[0].size();
-        dp=vector<vector<int>>(n,vector<int>(m,0));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i==0 && j==0){
-                    dp[i][j]=v[i][j];
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                if(j-1>=0){
+                    matrix[i][j]+=matrix[i][j-1];
                 }
-                else if(i==0){
-                    dp[i][j]=v[i][j]+dp[i][j-1];
-                }else if(j==0){
-                    dp[i][j]=v[i][j]+dp[i-1][j];
-                }else{
-                    dp[i][j]=dp[i-1][j]+dp[i][j-1]-dp[i-1][j-1]+v[i][j];
+                if(i-1>=0){
+                    matrix[i][j]+=matrix[i-1][j];
+                }
+                if(i-1>=0 && j-1>=0){
+                    matrix[i][j]-=matrix[i-1][j-1];
                 }
             }
         }
+        this->matrix=matrix;
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        if(row1==0 && col1==0){
-            return dp[row2][col2];
+        int x=matrix[row2][col2];
+        if(row1-1>=0){
+            x-=matrix[row1-1][col2];
         }
-        if(row1==0){
-            return dp[row2][col2]-dp[row2][col1-1];
+        if(col1-1>=0){
+            x-=matrix[row2][col1-1];
         }
-        if(col1==0){
-            return  dp[row2][col2]-dp[row1-1][col2];
+        if(row1-1>=0 && col1-1>=0){
+            x+=matrix[row1-1][col1-1];
         }
-        
-        return dp[row2][col2]-dp[row1-1][col2]-dp[row2][col1-1]+dp[row1-1][col1-1];
+        return x;
     }
 };
 
