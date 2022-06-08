25da47fa72467dc77ec1726bcd0175bd14b66b05
Amogh Nagar <amogh.1923cs1192@kiet.edu>
2022-06-08 19:50:59 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (26.79%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     int removePalindromeSub(string s) {
-        int f=0;
-        int l=0,h=s.length()-1;
-        while(l<=h){
-            if(s[l]!=s[h]){
-                f=2;
-                break;
+        int i=0,j=s.length()-1;
+        while(i<=j){
+            if(s[i++]!=s[j--]){
+                return 2;
             }
-            l++;
-            h--;
         }
-        if(l>h){
-            f=1;
-        }
-        return f;
+        return 1;
     }
 };
