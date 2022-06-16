247d234742e1f4bc09957788dee79c55ec8af6c2
Amogh Nagar <amogh.1923cs1192@kiet.edu>
2022-06-16 11:17:41 +0530

    Time: 1337 ms (5.63%), Space: 387.2 MB (5.33%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n=s.length(),m=0,pi=0,pj=0;
-        vector<vector<bool>> dp(n,vector<bool>(n,0));
-        for(int g=0;g<n;g++){
-            for(int i=0,j=g;j<n;i++,j++){
+        vector<vector<int>> dp(s.length(),vector<int>(s.length(),0));
+        int maxi=0,l=0;
+        for(int g=0;g<s.length();g++){
+            for(int i=0,j=g;j<s.length();j++,i++){
                 if(g==0){
-                    dp[i][j]=true;
+                    dp[i][j]=1;
                 }else if(g==1){
-                    
-                    dp[i][j]=s[i]==s[j];
+                    dp[i][j]=(s[i]==s[j]);
                 }else{
-                    dp[i][j]=s[i]==s[j] && dp[i+1][j-1];
+                    dp[i][j]=(s[i]==s[j] && dp[i+1][j-1]);
                 }
-                if(dp[i][j] && (j-i+1)>m){
-                    m=j-i+1;
-                    pj=j;
-                    pi=i;
+                if(dp[i][j]==1 && maxi<(j-i+1)){
+                    
+                     maxi=j-i+1;
+                     l=i;
+                    
                 }
             }
         }
-        return s.substr(pi,m);
+        return s.substr(l,maxi);
     }
 };
