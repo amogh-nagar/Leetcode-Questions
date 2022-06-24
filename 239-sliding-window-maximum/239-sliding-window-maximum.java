8419ada52a085f8e43348bcec418606e35d1bb3a
Amogh Nagar <amogh.1923cs1192@kiet.edu>
2022-06-24 13:39:34 +0530

    Time: 77 ms (25.34%), Space: 195 MB (5.00%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.java
+++ 239-sliding-window-maximum/239-sliding-window-maximum.java
@@ -1,45 +1,22 @@
 class Solution {
-public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<pair<int,int>> dq;
-        int i=0;
-        
-        while(k--){
-            if(dq.empty() || !dq.empty() && dq.front().first<nums[i]){
-             while(!dq.empty() && dq.front().first<nums[i])
-                 dq.pop_front();
-              dq.push_front({nums[i],i});
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        Deque<Integer> dq=new LinkedList<Integer>();
+        List<Integer> res=new ArrayList<Integer>();
+        for(int i=0;i<nums.length;i++){
+           
+            if(!dq.isEmpty() && i-k+1>dq.peekFirst()){
+                dq.removeFirst();
             }
-            else{
-                while(!dq.empty() && dq.back().first<nums[i])
-                    dq.pop_back();
-                
-                dq.push_back({nums[i],i});
-            }
-            i++;
-        }
-        int j=1;
-        vector<int> res;
-        res.push_back(dq.front().first);
-        for(;i<nums.size();i++,j++){
-            if(!dq.empty() && dq.front().second<j){
-                dq.pop_front();
-            }
+           
+                while(!dq.isEmpty() && nums[dq.peekLast()]<nums[i]){
+                    dq.removeLast();
+                }
+                dq.addLast(i);
             
-            if(!dq.empty() && dq.front().first<nums[i])
-            {
-                while(!dq.empty() && dq.front().first<nums[i])
-                 dq.pop_front();
-              dq.push_front({nums[i],i});
-            }
-            else{
-                while(!dq.empty() && dq.back().first<nums[i])
-                    dq.pop_back();
-                
-                dq.push_back({nums[i],i});
-            }
-            res.push_back(dq.front().first);
+            if(i>=k-1)
+            res.add(nums[dq.peekFirst()]);
         }
-        return res;
+        int[] arr = res.stream().mapToInt(i -> i).toArray();
+        return arr;
     }
-};
+}
